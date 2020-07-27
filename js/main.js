08ffc25188b0ecf1518262a6e094fb981ee9c638
DeeEssy <venchesters.2002@gmail.com>
2020-07-27 20:23:37 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+import {
+    radioPlayerInit
+} from "./radioPlayer.js"
+
+import {
+    videoPlayerInit
+}
+from "./videoPlayer.js"
+
+import {
+    musicPlayerInit
+}
+from "./musicPlayer.js"
+
+const playerBtn = document.querySelectorAll('.player-btn'),
+    playerBlock = document.querySelectorAll('.player-block'),
+    temp = document.querySelector('.temp')
+
+const deactivatianPlayer = () => {
+    temp.style.display = 'none'
+    playerBtn.forEach(item => {
+        item.classList.remove('active')
+    })
+    playerBlock.forEach(item => {
+        item.classList.remove('active')
+    })
+}
+
+playerBtn.forEach((btn, index) => {
+    btn.addEventListener('click', event => {
+        deactivatianPlayer()
+        btn.classList.add('active')
+        playerBlock[index].classList.add('active')
+    })
+})
